d8e5c4eebab2b95739b60c54769daa52fdb05e06
Fionn Orourke <G00429572@atu.ie>
2024-02-01 00:12:43 +0000

    health & gold

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 #functions
-def user_stats(pweapon, in1):
+def user_stats(pweapon, in1, health, gold):
     if in1 == "stats":
-        print("\n",pweapon[4],"\n\tDamage: ",pweapon[0],"\n\tChance of success: ", pweapon[1], "\n\tDurability: ", pweapon[2], "\n\tValue: ",pweapon[3])
+        print("\n",pweapon[4],"\n\tDamage: ",pweapon[0],"\n\tChance of success: ",
+               pweapon[1], "\n\tDurability: ", pweapon[2], "\n\tValue: ",pweapon[3],
+               "\n\nHealth: ",health, "\nGold: ", gold)
     
 
 
