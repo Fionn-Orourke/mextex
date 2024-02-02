51f0392dbb3cb21f99ee6cc48735ceef4b43042a
Fionn Orourke <G00429572@atu.ie>
2024-02-02 16:07:25 +0000

    random scene working

--- Scenarios.py
+++ Scenarios.py
@@ -1,28 +1,23 @@
 import random
+
 def sceneall():
-    scall = [scene1(), scene2(), scene3(), scene4()]
-    r = random.randint(0, 4)
-    print(r)
-
-    scall[r]
-
+    scall = [scene1, scene2, scene3, scene4]
+    r = random.choice(scall)
+    r()
+    return 0
 
 def scene1():
     print("1")
-
     return 0
 
 def scene2():
     print("2")
-
     return 0
 
 def scene3():
     print("3")
-
     return 0
 
 def scene4():
     print("4")
-
     return 0
