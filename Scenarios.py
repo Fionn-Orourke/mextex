90e48c08761e83c82dfdf1910109a187d4e23e55
Corosn <corofinsniper@hotmail.com>
2024-02-02 17:06:08 +0000

    first scene text

--- Scenarios.py
+++ Scenarios.py
@@ -7,17 +7,22 @@ def sceneall():
     return 0
 
 def scene1():
-    print("1")
+    print("Oh dear, you seem to have stumbled upon a rogue orc.")
+    in1 = input("Do you dare to try attack it")
     return 0
 
 def scene2():
-    print("2")
+    print("Proceed with caution, you sense something ahead of you.")
+    in1 = input("Do you wish to continue forward?")
     return 0
 
 def scene3():
-    print("3")
+    print("'You wouldn't happen to have some spare change for an old lady?" '')
+    in1 = input('"Would you?"')
+
     return 0
 
 def scene4():
-    print("4")
+    print('"Hello Traveller. Care to see whats in stock"')
+    in1 = input('Do you want to brwose his wares?')
     return 0
